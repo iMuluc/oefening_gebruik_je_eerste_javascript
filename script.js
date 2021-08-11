2131de0d020d89fdb3332faf0a1d79efcea87d3d
iMuluc <iMertUluc@gmail.com>
2021-08-12 00:15:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let mert = 'mert';
+console.log(mert);
+
+let getal = 4;
+let string = '4';
+
+console.log(getal + getal);
+console.log(string + string);
+console.log(getal - getal);
+console.log(getal * getal);
+console.log(getal / getal);
+console.log(getal % getal);
+
+let leeftijd = 21;
+console.log(typeof 21);
